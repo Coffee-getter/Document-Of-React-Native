9efc2db326e99854ecc9589bc86a0967831bae63
heye <heye@ewell.cc>
2022-06-27 10:21:15 +0800

    faker.js

---
+++ utils/faker.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: heye
+ * @Date: 2022-06-24 17:57:27
+ * @LastEditors: heye
+ * @LastEditTime: 2022-06-24 17:57:51
+ * @FilePath: \speed-up-cli\new-project\utils\faker.js
+ * @Description: 随机生成机地址
+ *
+ */
+const faker = require('faker');
+export default [...Array(200)].map((x) => faker.);
