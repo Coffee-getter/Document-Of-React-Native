a260466e2ae60364c2039f32e1ac6e4d03241343
heye <heye@ewell.cc>
2022-07-14 15:30:02 +0800

    About页面接收跨页面data

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -2,8 +2,8 @@
  * @Author: heye
  * @Date: 2022-06-30 10:40:52
  * @LastEditors: heye
- * @LastEditTime: 2022-07-01 10:38:11
- * @FilePath: \speed-up-cli\new-project\components\restaurantDetail\About.js
+ * @LastEditTime: 2022-07-14 14:32:39
+ * @FilePath: \new-project\components\restaurantDetail\About.js
  * @Description: 餐厅相关页
  * @argu
  * [base]图片基地址
@@ -12,27 +12,34 @@
  * [description] 格式化的餐厅描述
  *
  */
+import { PinDropSharp } from '@material-ui/icons';
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity, Image, Button } from 'react-native';
 
 const base = '../../assets/images';
 const yelpRestautantInfo = {
     name: 'Farmhouse Kitchen Thai Cuisine',
-    image: require(`${base}/about.jpg`),
+    image_url: require(`${base}/about.jpg`),
     price: '$$',
     reviews: '1500',
-    nating: 4.5,
-    categonies: [{ title: 'Thai' }, { title: 'Comfort Food' }],
+    rating: 4.5,
+    categories: [{ title: 'Thai' }, { title: 'Comfort Food' }],
 };
-export default function About() {
-    const { name, image, price, reviews, nating, categonies } = yelpRestautantInfo;
-    const formattedCategonies = categonies.map((cat) => cat.title).join(' · ');
+export default function About({ restaurantItem, navigation }) {
+    let { name, image_url, price, review_count, rating, categories } = restaurantItem;
+    const formattedCategonies = categories?.map((cat) => cat.title).join(' · ');
     const description = `${formattedCategonies} ${
         price ? ' · ' + price : null
-    }  ·  🎫  ·  ${nating}⭐(${reviews})+`;
+    }  ·  🎫  ·  ${rating}⭐(${review_count})+`;
+
+    const backToHome = () => {
+        navigation.navigate('Home');
+    };
+
     return (
         <View>
-            <RestaurantImage image={image} />
+            <BackButton backToHome={backToHome} />
+            <RestaurantImage image={yelpRestautantInfo.image_url} />
             <RestaurantTitle name={name} />
             <RestaurantDes description={description} />
         </View>
@@ -70,5 +77,25 @@ const RestaurantDes = (props) => {
         >
             <Text style={{ fontWeight: '500', color: '#555' }}>{props.description}</Text>
         </View>
+    );
+};
+
+const BackButton = (props) => {
+    return (
+        <TouchableOpacity
+            style={{
+                position: 'absolute',
+                top: 40,
+                left: 10,
+                backgroundColor: '#777',
+                borderRadius: 20,
+                zIndex: 999,
+                paddingVertical: 5,
+                paddingHorizontal: 15,
+            }}
+            onPress={props.backToHome}
+        >
+            <Text style={{ color: 'white' }}>返回</Text>
+        </TouchableOpacity>
     );
 };
